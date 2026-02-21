ecd512ec1efe1fc60851cd9206da8239453e4e08
Latrusanimi <stephan.fankhauser@bbz-sh.ch>
2026-02-21 11:13:09 +0100

    Vervollständigt

--- main.cpp
+++ main.cpp
@@ -10,15 +10,30 @@ struct Studierende {
 };
 
 int main() {
-
     vector<Studierende> students;
 
     // Füge 4 Studierende in die Liste students ein
+    students.push_back(Studierende{"Graf", 4.5});
+    students.push_back(Studierende{"Joe", 2.0});
+    students.push_back(Studierende{"Maria", 5.5});
+    students.push_back(Studierende{"Robert", 3.2});
 
     cout << "Pruefungsergebnisse:" << endl;
     // Gib alle Studierenden und die jeweilige Note aus
+    for (int i = 0; i < students.size(); i++) {
+        cout << students[i].name << " " << students[i].note << endl;
+    }
 
     // Gib aus, ob die jeweilige Person bestanden hat oder nicht
+    cout << "\nPruefungsergebnisse 2:" << endl;
+    for (int i = 0; i < students.size(); i++) {
+        if (students[i].note >= 4.0) {
+            cout << students[i].name << " " << students[i].note << " " << "Bestanden" << endl;
+        }
+        else {
+            cout << students[i].name << " " << students[i].note << " " << "Nicht Bestanden" << endl;
+        }
+    }
 
     return 0;
 }
